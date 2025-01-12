2bb9140c9fe0dd99fe6cffb0cfaee8b0afe41889
s-blu <git@s-blu.de>
2025-01-12 11:38:34 +0100

    refactor(scrollposition): add interface typing, clean up console logs
    quit early if scroll position is not 0, no need to search the data then

--- src/scrollposition.ts
+++ src/scrollposition.ts
@@ -3,8 +3,12 @@ import { RememberScrollpositionPluginData } from "./scrollposition.interface";
 export class RememberScrollposition {
 	scrollingDebounce: NodeJS.Timeout;
 
-	static saveScrollPosition(view, data, callback) {
-		console.log("Scrolling has stopped.");
+	static saveScrollPosition(
+		view,
+		data: RememberScrollpositionPluginData,
+		callback: (data: RememberScrollpositionPluginData) => void
+	) {
+		console.log("saving scroll position");
 		if (!view?.file) return;
 
 		const filepath = view.file.path;
@@ -29,26 +33,27 @@ export class RememberScrollposition {
 	}
 
 	static restoreScrollposition(view, data: RememberScrollpositionPluginData) {
-    if (!view ||!data) {
-      console.error('restoreScrollposition was called with invalid parameters.')
-      return;
-    }
+		if (!view || !data) {
+			console.error(
+				"restoreScrollposition was called with invalid parameters."
+			);
+			return;
+		}
+    console.log('restoring scroll position, current: ', view.editor.getScrollInfo()?.top)
 		const currentScrollPosition = view.editor.getScrollInfo()?.top;
-		// TODO check if there's a saved scroll position and restore the correct position
-		console.log(
-			"received view, current scrollposition:",
-			view.editor.getScrollInfo()?.top
-		);
+    // only try to set the scroll position if its on top. If its not, it was already updated before
+    if (currentScrollPosition !== 0) return;
 
-    const lastPosition = data.scrollpositions.find(p => p.path === view?.file?.path)?.scrollposition
+		const lastPosition = data.scrollpositions.find(
+			(p) => p.path === view.file?.path
+		)?.scrollposition;
 
-    // TODO check how old the scroll position is and ignore it if needed
+		// TODO check how old the scroll position is and ignore it if configured in settings
 
-		// only try to set the scroll position if its on top. If its not, it was already updated before
 		if (lastPosition && currentScrollPosition === 0) {
 			view.editor.scrollTo(null, lastPosition);
 			console.log(
-				"updated scrollposition:",
+				"updated scrollposition to:",
 				view.editor.getScrollInfo()?.top
 			);
 		}
