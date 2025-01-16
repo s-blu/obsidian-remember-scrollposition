450e7db29a932cf50fb9b2a6b7800a7a2faaf924
s-blu <git@s-blu.de>
2025-01-16 22:10:04 +0100

    test(project): reenable noImplicitAny

--- tests/remember-scrollposition.spec.ts
+++ tests/remember-scrollposition.spec.ts
@@ -1,9 +1,13 @@
-import { RememberScrollposition } from "../src/scrollposition"
+import { RememberScrollposition } from "../src/scrollposition";
 
-describe('RememberScrollposition', () => {
-  describe('saveScrollPosition', () => {
-    it('should do nothing if app is invalid', () => {
-      RememberScrollposition.saveScrollPosition(null, null, null)
-    })
-  })
-})
+describe("RememberScrollposition", () => {
+	describe("saveScrollPosition", () => {
+		it("should do nothing if app is invalid", () => {
+			RememberScrollposition.saveScrollPosition(
+				null as any,
+				null as any,
+				null as any
+			);
+		});
+	});
+});
