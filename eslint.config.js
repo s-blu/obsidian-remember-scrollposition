f4bc5bfb98424247fdab1a295b12ed6b4b008eaa
sam <git@s-blu.de>
2025-11-09 19:51:46 +0100

    chore(eslint): use typed eslinting

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,29 @@ import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores  } from "eslint/config";
 
 export default defineConfig([
+  
   { files: ["**/*.ts"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-    { files: ["**/*.spec.ts"], 
+    
+    {
       rules: {
     "@typescript-eslint/no-explicit-any": "off"
+  }
+},
+tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+      {
+        files: ["**/*.spec.ts"],
+      rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/no-unsafe-return": "off"
   }
 },
 globalIgnores([
